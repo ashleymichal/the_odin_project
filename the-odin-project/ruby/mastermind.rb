ece78a29e4c83e3c8f563f7bc4072b7272d3f2af
Ashley Michal <ashleymichal@gmail.com>
2015-08-21 16:47:04 -0500

    Built matching functions

--- the-odin-project/ruby/mastermind.rb
+++ the-odin-project/ruby/mastermind.rb
@@ -1,53 +1,77 @@
 class Mastermind
+  @@code_length == 4
+  @guesses = []
+  def initialize
+    # codemaker = PLayer.new <= later
+    codebreaker = Player.new
+    secret_code = Code.new(true) # if codemaker is computer
+  end
   
-
+  class Code
+    attr_reader :combo
+    def initialize(random=false)
+      unless random
+        make_custom
+      else
+        make_random
+      end
+    end
   
-end
-
-class Code
-  attr_reader :combo
-  # takes a string (combo) of 4 letters A..F
-  def initialize(random=false)
-    unless random
-      make_custom
-    else
-      make_random
+    private
+    def make_random
+      @combo = []
+      @@code_length.times do
+        @combo << rand(65..70).chr #random letter between A and F
+      end
+    end
+    def make_custom
+      begin
+        puts "Enter your secret code."
+        @combo = cleanup(gets)
+      rescue
+        puts $!
+        retry
+      end
+    end
+    def cleanup(combo)
+      combo = combo.gsub(/\s+/, '')
+      combo = combo.upcase
+      combo = combo.split('')
+      raise "Combo must be #{@code_length} letters long" unless combo.length == @code_length
+      unless combo.all? { |letter| ("A".."F").include?(letter) }
+        raise "Combo must contain only letters A-F"
+      end
+      combo
+    end
+    
+    def matches(guess)
+      # first find bulls
+      bulls = self.bulls(guess)
+      remaining_guess = []
+      guess.each_with_index do |letter, index|
+        remaining_guess << letter if bulls.include?(index)
+      end
+      # then find cows
+      cows = self.cows(remaining_guess)
+      return [bulls.length, cows]
+    end
+    
+    # returns an array of indexes corresponding to bulls
+    # remember that a guess is a Code object too, so.... yeah
+    def bulls(guess)
+      bulls = []
+      guess.each_with_index do |letter, index|
+        bulls << index if letter == self[index]
+      end
+      bulls
+    end
+  
+    # returns an integer representing the number of cows
+    def cows(guess)
+      cows = 0
+      cows
     end
   end
-  
-  def make_custom
-    begin
-      puts "Enter your secret code."
-      @combo = cleanup(gets)
-    rescue # another opportunity to play with different exceptions
-      puts $!
-      retry
-    end
-  end
-  
-  def make_random
-    @combo = []
-    4.times do
-      @combo << rand(65..70).chr #random letter between A and F
-    end
-  end
-
-  # check that all characters in combo are between A and F, and that combo is 4 long
-  def cleanup(combo)
-    combo.gsub!(/\s+/, '')
-    combo.upcase!
-    combo = combo.split('')
-    raise "Combo must be 4 letters long" unless combo.length == 4
-    unless combo.all? { |letter| ("A".."F").include?(letter) }
-      raise "Combo must contain only letters A-F"
-    end
-    combo
-  end
-  
-  def match
-    # TODO
-  end
-  
 end
 
 =begin NOTES
@@ -87,7 +111,7 @@ Classes:
 
 =end
 
-secret_code = Code.new
+secret_code = Mastermind::Code.new
 p secret_code.combo
-random_code = Code.new(true)
+random_code = Mastermind::Code.new(true)
 p random_code.combo
