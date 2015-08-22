5640921a6ca30ff8cae25bfcb532aeebfce8f6bc
Ashley Michal <ashleymichal@gmail.com>
2015-08-22 17:09:19 -0500

    Tested and refactored Code#matches

--- the-odin-project/ruby/mastermind.rb
+++ the-odin-project/ruby/mastermind.rb
@@ -1,13 +1,40 @@
 class Mastermind
-  @@code_length == 4
-  @guesses = []
-  def initialize
-    # codemaker = PLayer.new <= later
-    codebreaker = Player.new
-    secret_code = Code.new(true) # if codemaker is computer
+  attr_accessor :guesses
+  def initialize(player_name)
+    @guesses = []
+    @codemaker = PLayer.new
+    @secret_code = Code.new(true) # if codemaker is computer
+    @codebreaker = Player.new(name, true)
+  end
+  
+  def play
+    # print instructions
+    until victory(codebreaker) || guesses.length == maximum_guesses
+      # codebreaker makes a guess
+      guess = Code.new
+      if guess.matches == [@@code_length, 0]
+        victory(codebreaker)
+      end
+      guesses << guess
+      # show guesses, plus bulls and cows
+    end
+    victory(codemaker)
+  end
+  
+  def victory(player)
+    puts "#{player} wins!"
+    return
+  end
+  
+  class Player
+    def initialize(name="Computer", human=false)
+      @human = human
+      @name = name
+    end
   end
   
   class Code
+  @@code_length = 4
     attr_reader :combo
     def initialize(random=false)
       unless random
@@ -15,6 +42,19 @@ class Mastermind
       else
         make_random
       end
+    end
+    
+    # takes a guess and returns an array with the number of bulls (@0) and cows (@1)
+    def matches(guess)
+      # first find bulls
+      bulls = bulls(guess)
+      remaining_guess = []
+      guess.each_with_index do |letter, index|
+        remaining_guess << letter if bulls.include?(index)
+      end
+      # then find cows
+      cows = cows(remaining_guess)
+      return [bulls.length, cows]
     end
   
     private
@@ -24,6 +64,7 @@ class Mastermind
         @combo << rand(65..70).chr #random letter between A and F
       end
     end
+    
     def make_custom
       begin
         puts "Enter your secret code."
@@ -33,46 +74,46 @@ class Mastermind
         retry
       end
     end
+    
     def cleanup(combo)
       combo = combo.gsub(/\s+/, '')
       combo = combo.upcase
       combo = combo.split('')
-      raise "Combo must be #{@code_length} letters long" unless combo.length == @code_length
+      raise "Combo must be #{@@code_length} letters long" unless combo.length == @@code_length
       unless combo.all? { |letter| ("A".."F").include?(letter) }
         raise "Combo must contain only letters A-F"
       end
       combo
     end
     
-    def matches(guess)
-      # first find bulls
-      bulls = self.bulls(guess)
-      remaining_guess = []
-      guess.each_with_index do |letter, index|
-        remaining_guess << letter if bulls.include?(index)
-      end
-      # then find cows
-      cows = self.cows(remaining_guess)
-      return [bulls.length, cows]
-    end
-    
-    # returns an array of indexes corresponding to bulls
-    # remember that a guess is a Code object too, so.... yeah
+    # takes an array (Code.combo)
+    # returns an array of indexes corresponding to "bulls"
     def bulls(guess)
       bulls = []
       guess.each_with_index do |letter, index|
-        bulls << index if letter == self[index]
+        bulls << index if letter == @combo[index]
       end
       bulls
     end
-  
-    # returns an integer representing the number of cows
+    
+    # takes an array (Code.combo)
+    # returns an integer representing the number of "cows"
     def cows(guess)
       cows = 0
       cows
     end
   end
 end
+
+## Tests
+
+secret_code = Mastermind::Code.new
+p secret_code.combo
+random_code = Mastermind::Code.new(true) #random
+p random_code.combo
+
+p random_code.matches(secret_code.combo)
+
 
 =begin NOTES
 
@@ -111,7 +152,3 @@ Classes:
 
 =end
 
-secret_code = Mastermind::Code.new
-p secret_code.combo
-random_code = Mastermind::Code.new(true)
-p random_code.combo
