b3c2566c827e30ba0c3f32b23b792a0a54e75220
Ashley Michal <ashleymichal@gmail.com>
2015-10-17 19:16:50 -0500

    Begin refactoring to deal with unsorted data set

--- ruby/CS/tree.rb
+++ ruby/CS/tree.rb
@@ -4,21 +4,21 @@ class BinarySearchTree
 	# takes an array of data and turns it into a binary tree full of Node objects
 	# assumes data_set is sorted
 	def self.build_tree data_set, parent=nil
-		puts "-----"
+		# puts "-----"
 		case data_set.length
 		when 0
 			return nil
 		when 1
-			puts "Data set is 1 element"
+			# puts "Data set is 1 element"
 			return nil if data_set[0] == parent.value || data_set[0] == parent.parent.value
-			puts "New node created: #{data_set[0]}"
+			# puts "New node created: #{data_set[0]}"
 			return Node.new(data_set[0], parent)
 		else
 			middle = data_set.length / 2
-			puts "Data set is #{data_set.length} elements"
-			puts "The center element is #{data_set[middle]}"
+			# puts "Data set is #{data_set.length} elements"
+			# puts "The center element is #{data_set[middle]}"
 			current_node = Node.new(data_set[middle], parent)
-			puts "New node created: #{current_node.value}"
+			# puts "New node created: #{current_node.value}"
 			@root = current_node if parent.nil?
 			current_node.left_child=(build_tree(data_set[0...middle], current_node))
 			current_node.right_child=(build_tree(data_set[middle+1..-1], current_node))
@@ -36,5 +36,5 @@ class BinarySearchTree
 	end
 end
 
-data_set = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324].sort
+data_set = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 BinarySearchTree.build_tree(data_set)
