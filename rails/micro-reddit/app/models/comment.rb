d2bb8ef33c31e3699a9365b501f2e9c27a8d5a48
Ashley Michal <ashleymichal@gmail.com>
2016-01-08 16:44:54 -0600

    completed micro-reddit mini-project

--- rails/micro-reddit/app/models/comment.rb
+++ rails/micro-reddit/app/models/comment.rb
@@ -1,6 +1,7 @@
-class Post < ActiveRecord::Base
-	has_many :comments
+class Comment < ActiveRecord::Base
 	belongs_to :user
+	belongs_to :post
 	validates :user_id, presence: true
+	validates :post_id, presence: true
 	validates :content, presence: true
 end
