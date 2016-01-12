1724e556828fb56c08d1bc185f923416d3f14d59
Ashley Michal <ashleymichal@gmail.com>
2016-01-12 16:20:20 -0600

    established one-to-many relationship for Members and Posts

--- rails/clubhouse/db/migrate/20160112221717_create_posts.rb
+++ rails/clubhouse/db/migrate/20160112221717_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
     	t.text :content
+    	t.references :members, index: true, foreign_key: true
 
       t.timestamps null: false
     end
