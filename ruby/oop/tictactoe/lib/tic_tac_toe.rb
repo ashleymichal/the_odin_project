818baf13709cef5103c9e542665b2a7d3012e18c
ashleymichal <ashleymichal@gmail.com>
2016-03-20 20:01:54 -0500

    revamped game with tests, much better

--- ruby/oop/tictactoe/lib/tic_tac_toe.rb
+++ ruby/oop/tictactoe/lib/tic_tac_toe.rb
@@ -1,82 +1,90 @@
 class TicTacToe
-  @@space_names = [ :A1, :A2, :A3, :B1, :B2, :B3, :C1, :C2, :C3 ]
+  SPACES = [ :A1, :A2, :A3, :B1, :B2, :B3, :C1, :C2, :C3 ]
   @@winning_combinations = [
     [:A1, :A2, :A3], [:B1, :B2, :B3], [:C1, :C2, :C3],
     [:A1, :B1, :C1], [:A2, :B2, :C2], [:A3, :B3, :C3],
     [:A1, :B2, :C3], [:A3, :B2, :C1]
   ]
+  attr_reader :players, :active_player, :board
   def initialize
     # initialize 2 players, assign playerX to X and playerO to O
     player = Struct.new(:mark)
-    @playerX = player.new('X')
-    @playerO = player.new('O')
-    # initialize board
-    @spaces = []
-    construct_board
-  end
-  
-  def construct_board
-    @@space_names.each do |position|  
-      @spaces << Space.new(position)
-    end
+    @players = [player.new(:X), player.new(:O)]
+    @active_player = @players[0]
+    @board = Hash.new()
+    SPACES.each { |space| @board[space] = ' ' }
+    start
   end
   
   def place_mark(mark, space)
-    space.mark=(mark)
-  end
-  
-  def find_space_by_position(position)
-    @spaces.detect { |space| space.position == position.to_sym }
+    @board[space] = mark
   end
   
   def find_spaces_by_mark(mark)
-    marked_spaces = @spaces.select { |space| space.mark == mark.to_sym }
-    marked_spaces.map do |space|
-      space.position
-    end
+    spaces = @board.keys.select { |space| @board[space] == mark }
   end
   
   def board_full?
-    @spaces.none? { |space| space.mark == " " }
+    @board.values.none? { |space| space == ' ' }
   end
   
   def show_board
     puts "  1    2    3"
     puts "---------------"
     row = "A".ord
-    @spaces.each do |space|
-      print "[ #{space.mark} ]"
-      if space.position.to_s.include?("3")
+    @board.keys.each do |space|
+      print "[ #{@board[space]} ]"
+      if space.to_s.include?("3")
         puts "  #{row.chr}"
         puts "---------------"
         row += 1
       end
     end
   end
-  
-  def start
-    active_player = @playerX
-      until board_full?
-        show_board
-        puts "#{active_player.mark}'s turn"
-        place_mark(active_player.mark)
-        if victory?(active_player)
-          puts "#{active_player.mark}'s win!"
-          return
-        end
-        active_player == @playerX ? active_player = @playerO : active_player = @playerX
-      end
-      show_board
-      puts "Cats game, no one wins."
-      # cats-game if the board fills without a winnder
+
+  def select_space
+    begin
+      space = gets.chomp("Pick a space")
+      parse(space.strip)
+    rescue
+      puts "That doesn't appear to be a valid space"
+      retry
+    end
+  end
+
+  def parse(space)
+    parsed_space = space.upcase.split('').sort { |a, b| b <=> a }.join.to_sym
+    raise ArgumentError unless (SPACES.include?(parsed_space) && @board[parsed_space] == ' ')
+    parsed_space
+  end
+
+  def game_over(winner)
+    message = winner ? "#{winner.mark} wins!" : "Cat's game"
+    puts message
   end
 
   def victory?(player)
     @@winning_combinations.any? { |combo| combo - find_spaces_by_mark(player.mark) == [] }
   end
+    
+  def start
+    until board_full?
+      puts "Welcome to Tic Tac Toe!"
+      puts "It's #{@active_player.mark}'s turn!"
+      show_board
+      place_mark(@active_player.mark, select_space)
+      return game_over(@active_player) if victory?(@active_player)
+      @active_player = @players.find { |p| p != @active_player}
+    end
+    game_over(nil)
+  end
 end
 
 if __FILE__ == $0
-  new_game = TicTacToe.new
-  new_game.start
+  while true
+    new_game = TicTacToe.new
+    puts "would you like to play again? (Y/N)"
+    again = gets.chomp
+    break unless again.match(/y/i)
+  end
 end
--- ruby/oop/tictactoe/spec/tic_tac_toe_spec.rb
+++ ruby/oop/tictactoe/spec/tic_tac_toe_spec.rb
@@ -1,31 +1,82 @@
-require 'ostruct'
 require 'spec_helper'
 require 'tic_tac_toe'
 
 describe 'TicTacToe' do
-  before :each do
-    # start new game
-    @game = TicTacToe.new
-  end
+  let(:game) { TicTacToe.new }
 
   context 'game over' do
 
     context 'winning combo' do
+      after :each do
+        @spaces.each { |space| game.place_mark(:X, space) }
+        expect(game).to receive(:game_over)
+        expect(game).to receive(:select_space)
+        expect(game).to receive(:show_board)
+        expect(game.board_full?).to be false
+        game.start
+      end
 
-      it 'ends game when winning combo is made' do
-        [:A1, :A2, :A3].each { |space| @game.place_mark('X', space) }
-        expect(@game).to receive(game_over)
+      it 'ends game with 3 across' do
+        @spaces = [:A1, :A2, :A3]
+      end
+
+      it 'ends game with 3 down' do
+        @spaces = [:A1, :B1, :C1]
+      end
+
+      it 'ends game with 3 diagonal' do
+        @spaces = [:A1, :B2, :C3]
       end
 
     end
 
-    context 'cats game' do
+    context "cat's game" do
 
       it 'ends game when board is full' do
-        expect(@game).to receive(game_over)
-        TicTacToe.space_names.each do |space|
-          mark = ['X', 'O'].sample
-          @game.place_mark(mark, space)
+        expect(game).to receive(:game_over)
+        TicTacToe::SPACES.each do |space|
+          mark = [:X, :O].sample
+          game.place_mark(mark, space)
+        end
+        game.start
+      end
+
+    end
+
+  end
+
+  context "player interaction" do
+
+    context "selecting a space" do
+
+      context "invalid entry" do
+        it "rejects invalid spaces" do
+          space = " "
+          expect { game.parse(space) }.to raise_error(ArgumentError)
+        end
+
+        it "rejects already filled spaces" do
+          game.place_mark(:X, :A1)
+          space = "A1"
+          expect { game.parse(space) }.to raise_error(ArgumentError)
+        end
+      end
+
+      context "valid entry" do
+        after :each do
+          expect(game.parse(@space)).to eql(:A1)
+        end
+
+        it "accepts valid spaces" do
+          @space = "A1"
+        end
+
+        it "accepts number first" do
+          @space = "1A"
+        end
+
+        it "is case-insensitive" do
+          @space = "a1"
         end
       end
 
